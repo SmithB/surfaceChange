4e12d992aeeb8fdb2dbe8f20a7970aa5ce2cf2e1
Ben Smith <besmith@uw.edu>
2020-09-10 00:40:26 -0400

    make_1415_queue.py: converted input arguments to use argparse

--- make_1415_queue.py
+++ make_1415_queue.py
@@ -13,24 +13,44 @@ import scipy.ndimage as snd
 import sys
 import os
 import re
-step=sys.argv[1]
+import argparse
+
+
+
+# account for a bug in argparse that misinterprets negative agruents
+argv=sys.argv
+for i, arg in enumerate(argv):
+    if (arg[0] == '-') and arg[1].isdigit(): argv[i] = ' ' + arg
+
+
+parser=argparse.ArgumentParser(description="generate a list of commands to run ATL11_to_ATL15")
+parser.add_argument('step', type=str)
+parser.add_argument('defaults_file', type=str)
+parser.add_argument('--region_file', '-R', type=str)
+args=parser.parse_args()
+
+
 if len(sys.argv) > 2:
     defaults_file=os.path.abspath(sys.argv[2])
 
 
-if step not in ['centers', 'edges','corners']:
+if args.step not in ['centers', 'edges','corners']:
     raise(ValueError('argument not known'))
     sys.exit()
 
-xy_jako=(-160000, -2280000)
-
-#XR=[-440000., -120000.]; YR= [-1840000., -1560000.]
-XR, YR= [None, None]
-
+if args.region_file is not None:
+    line_re=re.compile('(..)\s*=\s*\[\s*(\S+),\s*(\S+)\s*]')
+    temp={}
+    with open(args.region_file,'r') as fh:
+        for line in fh:
+            m = line_re.search(line)
+            temp[m.group(1)]=[np.float(m.group(2)), np.float(m.group(3))]
+    XR=temp['XR']
+    YR=temp['YR']
 
 defaults_re=re.compile('(.*)\s*=\s*(.*)')
 defaults={}
-with open(defaults_file,'r') as fh:
+with open(args.defaults_file,'r') as fh:
    for line in fh:
        m=defaults_re.search(line)
        if m is not None:
@@ -40,7 +60,6 @@ with open(defaults_file,'r') as fh:
 
 Wxy=float(defaults['-W'])
 Hxy=Wxy/2
-
 
 mask_G=pc.grid.data().from_geotif(defaults['--mask_file'].replace('100m','1km'))
 mask_G.z=snd.binary_dilation(mask_G.z, structure=np.ones([20, 20], dtype='bool'))
@@ -52,29 +71,30 @@ except keyError:
 
 if not os.path.isdir(out_dir):
     os.mkdir(out_dir)
-step_dir=out_dir+'/'+step
+step_dir=out_dir+'/'+args.step
 if not os.path.isdir(step_dir):
     os.mkdir(step_dir)
 
-if step=='centers':
+if args.step=='centers':
     delta_x=[0]
     delta_y=[0]
     symbol='r*'
-elif step=='edges':
+elif args.step=='edges':
     delta_x=[-1, 0, 0, 1.]
     delta_y=[0, -1, 1, 0.]
     symbol='bo'
-elif step=='corners':
+elif args.step=='corners':
     delta_x=[-1, 1, -1, 1.]
     delta_y=[-1, -1, 1, 1.]
     symbol='ms'
+
 x0=np.unique(np.round(mask_G.x/Hxy)*Hxy)
 y0=np.unique(np.round(mask_G.y/Hxy)*Hxy)
 x0, y0 = np.meshgrid(x0, y0)
 
 xg=x0.ravel()
 yg=y0.ravel()
-good=(np.abs(mask_G.interp(xg, yg)-1)<0.1) & (np.mod(xg-xy_jako[0], Wxy)==0) & (np.mod(yg-xy_jako[1], Wxy)==0)
+good=(np.abs(mask_G.interp(xg, yg)-1)<0.1) & (np.mod(xg, Wxy)==0) & (np.mod(yg, Wxy)==0)
 
 if XR is not None:
     good &= (xg>=XR[0]) & (xg <= XR[1]) & (yg > YR[0]) & (yg < YR[1])
@@ -83,10 +103,7 @@ if XR is not None:
 xg=xg[good]
 yg=yg[good]
 #[xg, yg]=pad_bins([xg, yg], 2, [Wxy/2, Wxy/2])
-ind=np.argsort(np.abs(xg+1j*yg-xy_jako[0]-1j*xy_jako[1]))
-xg=xg[ind]
-yg=yg[ind]
-#plt.plot(xg, yg,'g.')
+
 queued=[];
 for xy0 in zip(xg, yg):
     for dx, dy in zip(delta_x, delta_y):  
@@ -95,13 +112,13 @@ for xy0 in zip(xg, yg):
             continue
         else:
             queued.append(tuple(xy1))
-        out_file=out_dir+'/%s//E%d_N%d.h5' % (step, xy1[0]/1000, xy1[1]/1000)  
+        out_file=out_dir+'/%s//E%d_N%d.h5' % (args.step, xy1[0]/1000, xy1[1]/1000)  
         if not os.path.isfile(out_file):
             #plt.plot(xy0[0], xy0[1],'r*')
             #if np.sqrt((xy1[0]-320000.)**2 + (xy1[1]- -2520000.)**2) > 2.e5:
             #    continue
             #plt.plot(xy1[0], xy1[1],symbol)
-            cmd='python3 ~/git_repos/surfaceChange/ATL11_to_ATL15.py %d %d --%s @%s ' % (xy1[0], xy1[1], step, defaults_file)
-            print('source activate ATL14; '+cmd +';'+cmd+' --calc_error_for_xy')
+            cmd='python3 ~/git_repos/surfaceChange/ATL11_to_ATL15.py %d %d --%s @%s ' % (xy1[0], xy1[1], args.step, defaults_file)
+            print('source activate IS2; '+cmd +';'+cmd+' --calc_error_for_xy')
             
 
