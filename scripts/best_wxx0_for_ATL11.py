5f486729079eed78412691fb4e8769b6ab396c6a
Ben Smith <besmith@uw.edu>
2021-10-27 16:35:46 -0400

    scripts/best_wxx0_for_ATL11.py: added options for multiple pairs in ATL11, carry out interpolations in a loglog domain

--- scripts/best_wxx0_for_ATL11.py
+++ scripts/best_wxx0_for_ATL11.py
@@ -8,10 +8,10 @@ import pointCollection as pc
 import h5py
 #import os
 import LSsurf
-#import re
+import re
 import sys
 
-def safe_interp(x, x0_in, y0_in):
+def safe_interp(x, x0_in, y0_in, loglog=False):
     y=np.NaN
     
     if x0_in[-1] < x0_in[0]:
@@ -26,35 +26,52 @@ def safe_interp(x, x0_in, y0_in):
         #print([i0, i1])
         #print( x0[[i0, i1]])
         #print( y0[[i0, i1]])
-        y=np.interp(x, x0[[i0, i1]], y0[[i0, i1]])
+        if loglog:
+            y=np.exp(np.interp(np.log(x), np.log(x0[[i0, i1]]), np.log(y0[[i0, i1]]) ))
+        else:
+            y=np.interp(x, x0[[i0, i1]], y0[[i0, i1]])
     except Exception:
         pass
     return y
 
-def read_ATL11_file(file, mask_file):
+def read_ATL11_file(file, mask_file, num_pairs=3):
+    
+    if num_pairs==3:
+        pairs=[1, 2, 3]
+    else:
+        pairs=[1]
+        
+    D11s=[]
+    for pair in pairs:
+        try:
+            D11=pc.ATL11.data().from_h5(file, pair=pair)
+            with h5py.File(file,'r') as h5f:
+                qs=np.array(h5f[f'/pt{pair}/ref_surf/fit_quality'])
+            D11.assign({'ref_surf_quality':qs})
+            D11.get_xy(EPSG=3413)
+            XR=np.array([np.nanmin(D11.x), np.nanmax(D11.x)])
+            YR=np.array([np.nanmin(D11.y), np.nanmax(D11.y)])
+            mask=pc.grid.data().from_geotif(mask_file, bounds=[XR, YR]).interp(D11.x[:,0], D11.y[:,0]) > 0.5
+            D11.index(mask & (D11.ref_surf_quality <1))
+            D11s += [D11]
+        except Exception as e:
+            print(e)
+            pass
+    return D11s
 
-    D11=pc.ATL11.data().from_h5(file)
-    with h5py.File(file,'r') as h5f:
-        qs=np.array(h5f['/pt2/ref_surf/fit_quality'])
-    D11.assign({'ref_surf_quality':qs})
-    D11.get_xy(EPSG=3413)
-    XR=np.array([np.nanmin(D11.x), np.nanmax(D11.x)])
-    YR=np.array([np.nanmin(D11.y), np.nanmax(D11.y)])
-    mask=pc.grid.data().from_geotif(mask_file, bounds=[XR, YR]).interp(D11.x[:,0], D11.y[:,0]) > 0.5
-    D11.index(mask & (D11.ref_surf_quality <1))
-    return D11
+def save_L_interp(L_interps, file):
+    D_list=[]
+    for L_interp in L_interps:
+        pt0=np.array(list(L_interp.keys()))
+        D={}
+        for field in L_interp[pt0[0]]:
+            D[field]=np.array([L_interp[pt][field] for pt in pt0])
+        D_list +=[pc.data().from_dict(D)]
+    pc.data().from_list(D_list).to_h5(file)
 
-def save_L_interp(L_interp, file):
-    pt0=np.array(list(L_interp.keys()))
-    D={}
-    for field in L_interp[pt0[0]]:
-        D[field]=np.array([L_interp[pt][field] for pt in pt0])
-    pc.data().from_dict(D).to_h5(file)
+def find_best_wxx0(D11, W_domain, mask_file):
 
-
-def find_best_wxx0(D11, mask_file):
-
-    scale_vals=np.array([ 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300])
+    scale_vals=np.array([ 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30, 100, 300])
 
     E_d3zdx2dt=0.0001
     E_d2z0dx2=0.006
@@ -63,17 +80,19 @@ def find_best_wxx0(D11, mask_file):
     data_gap_scale=2500
 
     # define the domain's width in x, y, and time
-    W={'x':4.e4,'y':200,'t':.2}
+    W={'x':W_domain,'y':200,'t':.2}
     # define the grid center:
     XR=np.nanmean(D11.x_atc)+np.array([-1, 1])*W['x']/2
     ctr={'x':XR[0]+W['x']/2., 'y':0., 't':0.}
     # define the grid spacing
     spacing={'z0':100, 'dz':100, 'dt':.1}
 
-    dN=np.ceil(W['x']/30).astype(int)
+    # reference points are every 60 m, but every third reference point
+    # is returned.
+    dN=np.ceil(W['x']/20).astype(int)
 
     L_interp={}
-    for pt0 in np.arange(D11.ref_pt[0,0]+dN/2, D11.ref_pt[-1,0], dN):
+    for pt0 in np.arange(D11.ref_pt[0,0]+dN/2, D11.ref_pt[-1,0], dN/2):
         ii=np.flatnonzero(np.abs(D11.ref_pt[:,0]-pt0)<3*dN/2)
         N_good=np.sum(np.isfinite(D11.h_corr[ii,:]), axis=0)
         if np.max(N_good)<0.9*dN:
@@ -116,20 +135,43 @@ def find_best_wxx0(D11, mask_file):
             L_curve['N'].append(d_ed.size)
         for key in L_curve.keys():
             L_curve[key] = np.array(L_curve[key])
-
-        L_interp[pt0] = {"w_for_r_of_1":safe_interp(1, L_curve['sigma_hat_s'], L_curve['wzz0']), 
-                        'w_for_r_10pct_above_min':safe_interp(1.1*L_curve['sigma_hat_s'].min(), L_curve['sigma_hat_s'], L_curve['wzz0']), 
+        try:         
+            N0 = safe_interp(1, L_curve['sigma_hat_s'], L_curve['N'], loglog=True)
+            w_for_r_of_1 = safe_interp(1, L_curve['sigma_hat_s'], L_curve['wzz0'], loglog=True)
+            w_for_r_10pct_above_min = \
+                safe_interp(1.1*L_curve['sigma_hat_s'].min(), L_curve['sigma_hat_s'], L_curve['wzz0'], loglog=True)
+            
+            L_interp[pt0] = {"w_for_r_of_1":w_for_r_of_1, 
+                        'w_for_r_10pct_above_min': w_for_r_10pct_above_min, 
+                        'sigma_hat_min':np.nanmin(L_curve['sigma_hat_s']),
+                        'F_data_r_of_1':N0/d_ed.size, 
                         'x': xy_ctr[0], 
                         'y': xy_ctr[1],
-                        'z': xy_ctr[2]}
+                        'z': xy_ctr[2],
+                        'ref_pt': pt0}
+            #print([ L_interp[pt0]['w_for_r_of_1'], np.min(L_curve['sigma_hat_s'])])
+        except Exception as e:
+            print(e)
+
     return L_interp
 
 
 ATL11_file=sys.argv[1]
-mask_file=sys.argv[2]
-out_file=sys.argv[3]
-D11=read_ATL11_file(ATL11_file, mask_file)
-if D11.size > 0:
-    L_interp=find_best_wxx0(D11, mask_file)
-if len(list(L_interp.keys())) > 0:
-    save_L_interp(L_interp, out_file)
+W=float(sys.argv[2])
+mask_file=sys.argv[3]
+out_file=sys.argv[4]
+
+if len(sys.argv)>5:
+    num_pairs=sys.argv[5]
+else:
+    num_pairs=3
+
+D11s=read_ATL11_file(ATL11_file, mask_file, num_pairs)
+L_interps=[]
+for D11 in D11s:
+    if D11.size > 0:
+        L_interp = find_best_wxx0(D11, W, mask_file)
+    
+    if len(list(L_interp.keys())) > 0:
+        L_interps += [L_interp]
+save_L_interp(L_interps, out_file)
