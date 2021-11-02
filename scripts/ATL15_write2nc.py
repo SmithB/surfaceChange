63eb540232293196a536c1e5f3ca74bd99c21fd7
Benjamin P. Jelley <benjamin.p.jelley@nasa.gov>
2021-11-02 07:26:07 -0400

    ATL15 write attrs

--- scripts/ATL15_write2nc.py
+++ scripts/ATL15_write2nc.py
@@ -303,7 +303,7 @@ def ATL15_write2nc(args):
                     pass
         
             ncTemplate="atl14_metadata_template.nc"
-            write_atl14meta(nc, fileout, ncTemplate)
+            write_atl14meta(nc, fileout, ncTemplate, args)
 
     return fileout
     
@@ -324,6 +324,8 @@ if __name__=='__main__':
     parser.add_argument('-c','--cycles', type=str, help="4-digit number specifying first/last cycles for output filename")
     parser.add_argument('-R','--Release', type=str, help="3-digit release number for output filename")
     parser.add_argument('-v','--version', type=str, help="2-digit version number for output filename")
+    parser.add_argument('-list11','--ATL11_lineage_dir', type=str, help='directory in which to look for ATL11 .h5 filenames')
+    parser.add_argument('-tiles','--tiles_dir', type=str, help='directory in which to look for tile .h5 files')
     args, unknown = parser.parse_known_args()
     print('args',args)
     
